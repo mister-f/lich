10ae7dbc6b769d315266f4ff0330dfc177c8dbc7
Andrew Funk <funkand@oregonstate.edu>
2019-05-26 01:21:04 -0700

    Password reset added

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars').create({defaultLayout:'main'});
 const pool = require('./database');
 const path = require('path');
+const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
+const async = require('async');
+const crypto = require('crypto');
 
 const app = express();
 
@@ -59,6 +63,122 @@ app.post('/auth', function(req, res) {
 		res.send('Please enter Username and Password!');
 		res.end();
 	}
+});
+
+app.get('/forgot', function(req, res) {
+	res.render('forgot');
+});
+
+app.post('/forgot', function(req, res, next) {
+	async.waterfall([
+		function(done) {
+			crypto.randomBytes(20, function(err, buf) {
+				var token = buf.toString('hex');
+				done(err, token);
+			});
+		},
+		function(token, done) {
+			var email = req.body.email;
+			pool.query('SELECT * FROM employees WHERE email = ?', [email], function(err, results, fields) {
+				if (results.length > 0) {
+					var date = new Date();
+					date.setTime(date.getTime() + (60 * 60 * 1000));
+					date.toISOString().slice(0, 19).replace('T', ' ');
+					pool.query('UPDATE employees SET token = ?, tokenexpire = ? WHERE email = ?', [token, date, email], function (err, results, fields) {
+						console.log('Token added to user.');
+						done(err, token, email);
+					});
+				} else {
+					console.log('Invalid email entered.');
+				}
+
+      			});
+		},
+		function(token, email, done) {
+			const transporter = nodemailer.createTransport(sgTransport({
+				auth: {
+					api_user: 'funkand@oregonstate.edu',
+					api_key: '#2XkT5#UTVW3hx%8ce'
+				},
+			}));
+			const options = {
+				to: email,
+        			from: 'noreply@certifilich.com',
+				subject: 'Password Reset',
+        			text: 'You are receiving this because a password reset requested for your account.\n\n' +
+				'Click on the following link to complete the process:\n\n' +
+				'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+				'If you did not request this, please ignore this email.\n'
+			};
+			transporter.sendMail(options, function(err) {
+				console.log('Reset email sent.');
+				done(err);
+			});
+		}
+	], function(err) {
+		if (err) return next(err);
+		res.redirect('/forgot');
+	});
+});
+
+app.get('/reset/:token', function(req, res) {
+	pool.query('SELECT * FROM employees WHERE token = ?', [req.params.token], function(err, results, fields) {
+		if (results.length > 0) {
+			var user = JSON.parse(JSON.stringify(results));
+			curDate = new Date();
+			curDate.toISOString().slice(0, 19).replace('T', ' ');
+			if (curDate > user.tokenexpire) {
+				console.log('Expired token!');
+				return res.redirect('/forgot');
+			} else {
+				res.render('reset');
+		
+			}
+		} else {
+			console.log('Invalid token!');
+			return res.redirect('/forgot');
+		}
+	});
+});
+
+app.post('/reset/:token', function(req, res) {
+        async.waterfall([
+                function(done) {
+			pool.query('SELECT * FROM employees WHERE token = ?', [req.params.token], function(err, results, fields) {
+				var user = JSON.parse(JSON.stringify(results));
+				var email = user[0].email;
+				done(err, email);
+			});
+		},
+		function(email, done) {
+			var password = req.body.password;
+			pool.query('UPDATE employees SET token = NULL, tokenexpire = NULL, password = ? WHERE token = ?', [password, req.params.token], function (err, results, fields) {
+				console.log('Password Updated');
+				done(err, email);
+                        });
+                },
+                function(email, done) {
+                        const transporter = nodemailer.createTransport(sgTransport({
+                                auth: {
+                                        api_user: 'funkand@oregonstate.edu',
+                                        api_key: '#2XkT5#UTVW3hx%8ce'
+                                },
+                        }));
+                        const options = {
+                                to: email,
+                                from: 'noreply@certifilich.com',
+                                subject: 'Password Has Been Changed',
+                                text: 'Hello,\n\n' +
+					'This is a confirmation that the password for ' + email + ' has just been changed.\n'
+			};
+                        transporter.sendMail(options, function(err) {
+                                console.log('Confirmation email sent.');
+                                done(err);
+			});
+                }
+        ], function(err) {
+                res.redirect('/');
+        });
 });
 
 app.use('/employees', require('./routes/employees'));
